6d66aeeba77eea922843ed2d87adeea1721d1a85
Shlomo Swidler <shlomo.swidler@gmail.com>
2020-12-09 06:37:36 +0900

    more thorough example.sh

--- example.sh
+++ example.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
-awk -f blankify.awk madness=7 grammified.txt > blankTemplate.txt
-awk -f adlib.awk dict.txt blankTemplate.txt > filledTemplate.txt
-awk -f fill.awk grammified.txt filledTemplate.txt
+# create the dictionary from the recipe
+# grammify.sh pancakeRecipe.txt | awk -f dictionarize.awk > dict.txt
+
+# generate the mad libs
+# grammify.sh source.txt > grammified.txt
+echo "-- default madness level (2)"
+awk -f blankify.awk grammified.txt | awk -f adlib.awk dict.txt - | awk -f fill.awk grammified.txt -
+echo "-- madness level 5"
+awk -f blankify.awk madness=5 grammified.txt | awk -f adlib.awk dict.txt - | awk -f fill.awk grammified.txt -
+echo "-- madness level 0"
+awk -f blankify.awk madness=0 grammified.txt | awk -f adlib.awk dict.txt - | awk -f fill.awk grammified.txt -
+echo "-- madness level 10"
+awk -f blankify.awk madness=10 grammified.txt | awk -f adlib.awk dict.txt - | awk -f fill.awk grammified.txt -
